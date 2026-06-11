b5a8c7afa4e39ac2ab76e8ea4b8d59c1ffb17858
unknown <sandeepmanchikanti84@gmail.com>
2026-06-11 07:39:47 +0530

    Add QA chatbot application

---
+++ qa-chatbot/qa-chatbot.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+import streamlit as st
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLEKEY")
+
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=GOOGLE_API_KEY,
+    temperature=0,
+    max_completion_tokens=200
+)
+
+
+def LLM_Response(question):
+    response = llm.invoke(question)
+    return response.content
+
+### simple streamlit application
+
+
+st.title("Simple Q&A Chatbot")
+
+question = st.text_input("Enter your question:")
+
+if st.button("Ask"):
+    if question:
+        answer = LLM_Response(question)
+        st.subheader("Answer:")
+        st.write(answer)
+    else:
+        st.warning("Please enter a question.")
+
+
+## strealit application
